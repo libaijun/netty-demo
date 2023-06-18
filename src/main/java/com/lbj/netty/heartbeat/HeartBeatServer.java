a4385b77141505113b2be5b32e5ec575ea12626e
libaijun <baijun1027@sina.com>
2023-06-18 22:22:58 +0800

    netty日志级别

--- src/main/java/com/lbj/netty/heartbeat/HeartBeatServer.java
+++ src/main/java/com/lbj/netty/heartbeat/HeartBeatServer.java
@@ -8,6 +8,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 
@@ -32,7 +33,7 @@ public class HeartBeatServer {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup, workGroup)
                     .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler())
+                    .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
